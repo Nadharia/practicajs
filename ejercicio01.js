3f9df2f9bc7712d61eeaf0e61f8ec12a560069ce
Joako <87424697+Nadharia@users.noreply.github.com>
2025-06-13 23:22:15 -0300

    Add initial implementations for persona and frutas objects

---
+++ ejercicio01.js
@@ -0,0 +1,9 @@
+const persona = {
+  nombre: 'Joako',
+  edad: 25,
+  profesion: 'Desarrollador'
+};
+
+const {nombre,edad}=persona
+console.log(`${nombre} tiene ${edad}`)
+
---
+++ ejercicio02.js
@@ -0,0 +1,6 @@
+const frutas = ['manzana', 'banana', 'naranja', 'sandía'];
+
+
+const[a,b,c,d]=frutas
+
+console.log(`${a} ${b}`)
